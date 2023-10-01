a003c3e2b8ec093765294051fab07b65ecba79f5
Ethan_Wu_MacBookM1Max <cwu1@drew.edu>
2023-10-01 15:56:19 -0400

    update

--- hello_flask/app2.py
+++ hello_flask/app2.py
@@ -8,7 +8,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template("home.html")
+    
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
 
 
 # @app.route("/hello/<name>")
@@ -37,7 +45,7 @@ def hello_there(name = None):
         date=datetime.now()
     )
 
-    
+
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")

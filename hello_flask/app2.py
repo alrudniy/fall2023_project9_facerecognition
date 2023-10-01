4e49ff43ccf836b25a31c033e6c83d6081f2ea5a
Ethan_Wu_MacBookM1Max <cwu1@drew.edu>
2023-10-01 15:47:01 -0400

    update

--- hello_flask/app2.py
+++ hello_flask/app2.py
@@ -36,3 +36,8 @@ def hello_there(name = None):
         name=name,
         date=datetime.now()
     )
+
+    
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")

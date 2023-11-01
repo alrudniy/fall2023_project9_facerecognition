8c2916e022a4168ecd5e97fdf9bcc994490b8665
Ethan_Wu_MacBookM1Max <cwu1@drew.edu>
2023-11-01 12:10:58 -0400

    update

---
+++ flutter_340/android/app/src/main/kotlin/com/example/flutter_340/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_340
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- flutter_part/vscode_340/android/app/src/main/kotlin/com/example/vscode_340/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vscode_340
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
